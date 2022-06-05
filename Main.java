4129d386333cbfe77226cb944d7b823ac14b3784
and1qa <1111>
2022-06-05 21:48:57 +0300

    #5 - Данные от пользователя

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+
+        //byte num = 23; // -128 do 127
+        //float num_1 = 3.14f;
+        //boolean isTrue = true;
+
+        Scanner str = new Scanner(System.in);
+        System.out.println("Your string is: " + str.nextLine());
+    }
+}
