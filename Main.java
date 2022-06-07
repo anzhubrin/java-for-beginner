f11e8a6ce58ab18715368d4573d8a91e98d6dbcb
and1qa <1111>
2022-06-07 21:13:02 +0300

    #7 - Условные операторы - 2

--- Main.java
+++ Main.java
@@ -3,25 +3,40 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) {
 
+
+
+        /* 6 Занятие
         Scanner num = new Scanner(System.in);
         int first;
+        int second;
+        boolean isTrue = true;
         System.out.print("Enter first num: ");
         first = num.nextInt();
 
-
-        if (first == 10) {
+        if (first == 10 && isTrue) {
             System.out.print("Num is 10");
-            System.out.print("/n");
+        } else if (first == 5) {
+            System.out.print("Num is 5");
         } else {
-            System.out.print("Num not 10");
+            System.out.print("Num not 10 and 5");
         }
+
+
+        System.out.print("\n");
+        System.out.print("Enter second num: ");
+        second = num.nextInt();
+        switch (second) {
+            case 51: System.out.print("equal 51"); break;
+            case 31: System.out.print("equal 31"); break;
+            default: System.out.print("not equal");
+        } */
 
 
 
         /* 6 Занятие
         Scanner num = new Scanner(System.in);
-
         int first, second, result;
+
         System.out.print("Enter first num: ");
         first = num.nextInt();
         System.out.print("Enter second num: ");
@@ -29,6 +44,5 @@ public class Main {
 
         result = first + second;
         System.out.print("Summa is: " + result); */
-
     }
 }
