8ec669c31fa8ad5f308b7c74a746f400ca629421
and1qa <1111>
2022-06-12 10:48:22 +0300

    #9 - Массивы

--- Main.java
+++ Main.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main (String[] args) {
 
+        int[] mass = new int[] {12,23,43}; // Обьявление массива
+        System.out.println(mass[1]);
+        float[] arr = new float[] {12.3f,23.6f,43.6f}; // Обьявление массива
+        System.out.println(arr[1]);
+
+
+        //* 8 Занятие
 //        for (int i = 1 ; i <= 5; i++) {
 //            System.out.println(i);
 //        }
@@ -19,44 +26,44 @@ public class Main {
 
 
 
-        /* 7 Занятие
-        Scanner num = new Scanner(System.in);
-        int first;
-        int second;
-        boolean isTrue = true;
-        System.out.print("Enter first num: ");
-        first = num.nextInt();
+        //7 Занятие
+//        Scanner num = new Scanner(System.in);
+//        int first;
+//        int second;
+//        boolean isTrue = true;
+//        System.out.print("Enter first num: ");
+//        first = num.nextInt();
+//
+//        if (first == 10 && isTrue) {
+//            System.out.print("Num is 10");
+//        } else if (first == 5) {
+//            System.out.print("Num is 5");
+//        } else {
+//            System.out.print("Num not 10 and 5");
+//        }
 
-        if (first == 10 && isTrue) {
-            System.out.print("Num is 10");
-        } else if (first == 5) {
-            System.out.print("Num is 5");
-        } else {
-            System.out.print("Num not 10 and 5");
-        }
-
-
-        System.out.print("\n");
-        System.out.print("Enter second num: ");
-        second = num.nextInt();
-        switch (second) {
-            case 51: System.out.print("equal 51"); break;
-            case 31: System.out.print("equal 31"); break;
-            default: System.out.print("not equal");
-        } */
+//        System.out.print("Enter second num: ");
+//        second = num.nextInt();
+//        switch (second) {
+//            case 51: System.out.print("equal 51"); break;
+//            case 31: System.out.print("equal 31"); break;
+//            default: System.out.print("not equal");
+//        }      System.out.print("\n");
 
 
 
-        /* 6 Занятие
-        Scanner num = new Scanner(System.in);
-        int first, second, result;
 
-        System.out.print("Enter first num: ");
-        first = num.nextInt();
-        System.out.print("Enter second num: ");
-        second = num.nextInt();
 
-        result = first + second;
-        System.out.print("Summa is: " + result); */
+        //6 Занятие
+//        Scanner num = new Scanner(System.in);
+//        int first, second, result;
+//
+//        System.out.print("Enter first num: ");
+//        first = num.nextInt();
+//        System.out.print("Enter second num: ");
+//        second = num.nextInt();
+//
+//        result = first + second;
+//        System.out.print("Summa is: " + result);
     }
 }
